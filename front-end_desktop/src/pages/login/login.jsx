29f01cf84911da561a2741cfb7d9316478268ece
appsoft-lucio <luciocdesouza@gmail.com>
2025-01-03 11:07:17 -0300

    Cria função para autenticar usuário

--- front-end_desktop/src/pages/login/login.jsx
+++ front-end_desktop/src/pages/login/login.jsx
@@ -6,13 +6,42 @@ import logo from "../../assets/logo.png";
 import fundo from "../../assets/fundo.png";
 import olhoAberto from "../../assets/olhoAberto.png";
 import olhoFechado from "../../assets/olhoFechado.png";
+import api from "../../constants/api.js";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alert, setalert] = useState("");
 
-  function ExecuteLogin() {
-    navigate("/appointments");
+  async function ExecuteLogin(e) {
+    e.preventDefault();
+    setalert("");
+    try {
+      const response = await api.post("/users/login", { email, password });
+
+      if (response.data) {
+        localStorage.setItem("sessionToken", response.data.token);
+        localStorage.setItem("sessionId", response.data.id_user);
+        localStorage.setItem("sessionEmail", response.data.email);
+        localStorage.setItem("sessionName", response.data.name);
+        api.defaults.headers.common["Authorization"] =
+          "Bearer" + response.data.token;
+        navigate("/appointments");
+      } else {
+        setalert("Erro ao efeutar login. Tente novamente");
+        console.log("Erro ao efeutar login. Tente novamente");
+      }
+    } catch (error) {
+      if (error.response?.data.error) {
+        setalert(error.response?.data.error);
+      } else {
+        setalert("Erro ao efeutar login. Tente novamente");
+      }
+      console.log(error);
+    }
+    console.log(password, email);
   }
   return (
     <div className="row login-container">
@@ -24,12 +53,19 @@ export default function Login() {
           </h4>
           <h5 className="mb-2 text-secondary">Acesse sua conta.</h5>
           <div className="mt-4">
-            <input type="email" placeholder="E-mail" />
+            <input
+              type="email"
+              placeholder="E-mail"
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete="username"
+            />
           </div>
           <div className="mt-3 ">
             <input
               type={showPassword ? "text" : "password"}
               placeholder="Senha"
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
             />
             <img
               src={showPassword ? olhoAberto : olhoFechado}
@@ -40,10 +76,15 @@ export default function Login() {
             />
           </div>
           <div className="mt-3">
-            <button onClick={ExecuteLogin} className="btn btn-custom">
+            <button onClick={ExecuteLogin} className="btn btn-custom mb-3">
               Acessar
             </button>
           </div>
+          {alert.length > 0 ? (
+            <div className="alert alert-danger" role="alert">
+              {alert}
+            </div>
+          ) : null}
           <div className="mt-5 mb-3">
             <span>Não tenho conta. </span>
             <Link to="/register">Criar conta.</Link>
