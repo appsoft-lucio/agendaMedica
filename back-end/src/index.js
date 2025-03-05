d24014513a5fb08dea79f4128e081378fd44cf27
appsoft-lucio <luciocdesouza@gmail.com>
2025-03-04 21:13:26 -0300

    Ajustado porta para 8080 no Fly.io

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.listen(3001, () => {
-  console.log("Servidor rodando na porta: 3001.");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta: ${PORT}`);
 });
